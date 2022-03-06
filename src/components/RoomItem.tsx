42d7a550a5f18bb8ca20ca37f531ecd844177038
sararekowska <sara.rekowska@gmail.com>
2022-03-06 18:01:28 +0100

    feat: filtering by occupancy

--- src/components/RoomItem.tsx
+++ src/components/RoomItem.tsx
@@ -4,22 +4,29 @@ import { Data } from "../common/types/types";
 import { UIContext } from "./UIContext";
 
 export const RoomItem = (props: { data: Data }) => {
-  const store = useContext(UIContext);
+  const { maxAdult, maxChild } = useContext(UIContext);
   return (
     <div>
-      {props.data?.rooms.map((room: Room) => (
-        <div
-          key={room.id}
-          className="border-t-2 border-[#634d44] p-8 flex flex-row"
-        >
-          <div className="w-[20%] mr-8">
-            <h1 className="font-bold text-xl">{room.name}</h1>
-            <p className="pt-2">Adults: {room.occupancy.maxAdults}</p>
-            <p>Children: {room.occupancy.maxChildren}</p>
+      {props.data?.rooms
+        .filter((room: Room) =>
+          maxAdult === 0 && maxChild === 0
+            ? true
+            : room.occupancy.maxAdults === maxAdult &&
+              room.occupancy.maxChildren === maxChild
+        )
+        .map((room: Room) => (
+          <div
+            key={room.id}
+            className="border-t-2 border-[#634d44] p-8 flex flex-row"
+          >
+            <div className="w-[20%] mr-8">
+              <h1 className="font-bold text-xl">{room.name}</h1>
+              <p className="pt-2">Adults: {room.occupancy.maxAdults}</p>
+              <p>Children: {room.occupancy.maxChildren}</p>
+            </div>
+            <p className="w-[75%]">{room.longDescription}</p>
           </div>
-          <p className="w-[75%]">{room.longDescription}</p>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
